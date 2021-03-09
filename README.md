2487acd1e7d3b1946105e2ac53635426ac215e9a
Abhinav Joshi <abhinavmjoshi@gmail.com>
2021-03-09 17:52:29 -0500

    finished functionality for all websites

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
 # pharma-news-scraper
 
 Web Scraper for latest pharmaceutical company news - Investing Tool
-
-List of included company url's (* entries are currently incompatible with scraper program, # entries were removed (incompatible), but may be readded)
-*ADITX Therapeutics*
-Gristone Oncology
-*Kintara Therapeutics*
-Moderna
-Novan
-*Ocugen*
-*Onconova*
-# *Solid Life Biosciences* 
-Tiziana Life Sciences
-Zomedica
--- scraper.py
+++ scraper.py
@@ -27,14 +27,58 @@ f = open("results.txt", "w")
 f.write("Pharma Companies News and Releases for Today\n")
 
 for url in currentline:
-    print('Processing URL:', url)  
+    print('Processing URL:', url)
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     # Make the BS requests first to get the data
-    curr_date_datetime = str(soup.find_all(
-        "div", {"class": "nir-widget--field nir-widget--news--date-time"})[0].string)
-    curr_date_headline = str(soup.find_all(
-        "div", {"class": "nir-widget--field nir-widget--news--headline"})[0].string)
+    if url == "https://investor.onconova.com/press-releases":
+        curr_date_datetime = str(soup.find_all(
+            "time", {"class": "datetime"})[0].string)
+        curr_date_results = soup.find_all(
+            "td", {"headers": "view-field-nir-news-title-table-column"})
+        curr_date_headline = ""
+        index = 0
+        for tag in curr_date_results:
+            if index > 0:
+                break
+            aTags = tag.find_all("a")[0].string
+            res = re.split(', |>|<|!', aTags)
+            index += 1
+            curr_date_headline = res[0].split("\n")[0].strip()
+    elif url == "https://ir.ocugen.com/news-releases":
+        curr_date_datetime = str(soup.find_all(
+            "div", {"class": "nir-widget--field nir-widget--news--date-time"})[0].string)
+        curr_date_results = soup.find_all(
+            "article", {"role": "article"})
+        curr_date_headline = ""
+        index = 0
+        for tag in curr_date_results:
+            if index > 0:
+                break
+            aTags = tag.find_all("a")[0].string
+            res = re.split(', |>|<|!', aTags)
+            index += 1
+            curr_date_headline = res[0].split("\n")[0].strip()
+    elif url == "https://ir.kintara.com/press-releases":
+        curr_date_datetime = str(soup.find_all(
+            "div", {"class": "date"})[0].string)
+        curr_date_results = soup.find_all(
+            "h2", {"class": "media-heading"})
+        curr_date_headline = ""
+        index = 0
+        for tag in curr_date_results:
+            if index > 0:
+                break
+            aTags = tag.find_all("a")[0].string
+            res = re.split(', |>|<|!', aTags)
+            index += 1
+            curr_date_headline = res[0].split("\n")[1].strip()
+    else:
+        curr_date_datetime = str(soup.find_all(
+            "div", {"class": "nir-widget--field nir-widget--news--date-time"})[0].string)
+        curr_date_headline = str(soup.find_all(
+            "div", {"class": "nir-widget--field nir-widget--news--headline"})[0].string)
+
     # If the article's date matches today, enter logic to add results to solution
     if parser.parse(curr_date_datetime.strip()).date() == datetime.today().date():
         # If we find an article relating to today, add to the dictionary
